64ae0eee3ea3489d4e0c4b330cbeb42c1f0f1ab5
azildest <azildest@users.noreply.github.com>
2022-12-20 07:51:46 +0700

    initiate commit project

---
+++ AppBangunDatar/src/main/java/com/mycompany/appbangundatar/BangunDatar.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.appbangundatar;
+
+/**
+ *
+ * @author acer
+ * */
+
+ abstract class BangunDatar {
+     private String namaBangunDatar;
+
+    public BangunDatar() {
+    }
+
+    public String getNamaBangunDatar() {
+        return namaBangunDatar;
+    }
+
+    public void setNamaBangunDatar(String namaBangunDatar) {
+        this.namaBangunDatar = namaBangunDatar;
+    }
+    
+    abstract double luas();
+    
+    abstract double keliling();
+}
